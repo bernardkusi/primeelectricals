9f242aaeb413b300b3f383ea98ba10750834ae47
Khuccy <bernardkusi25@gmail.com>
2022-09-09 19:59:55 +0000

    Added all images

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,23 +3,43 @@ import React from 'react'
 const Rating = ({rating}) => {
     if(rating===5){
         return (
-            <span>#####</span>
+            <span>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+            </span>
           )
     }else if(rating===4){
         return (
-            <span>####</span>
+            <span>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+            </span>
           )
     }else if(rating===3){
         return (
-            <span>###</span>
+            <span>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+            </span>
           )
     }else if(rating===2){
         return (
-            <span>##</span>
+            <span>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+            </span>
           )
     }else{
         return (
-            <span>#</span>
+            <span>
+                <i class="fa-solid fa-star"></i>
+            </span>
           )
     }
   
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,56 +1,86 @@
-import React from 'react'
+import React from "react";
 
 const Services = () => {
   return (
-    <div id="services" style={{backgroundImage:" linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)), url(./services.jpg)"}}>
-        <div className="page">
-            <p className="heading">Our Services</p>
+    <div
+      id="services"
+      style={{
+        backgroundImage:
+          " linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)), url(./services.jpg)",
+      }}
+    >
+      <div className="page">
+        <p className="heading">Our Services</p>
 
-            <div className="serviceflex">
-                <div className="service">
-                  <div className="service2">
-                    <div className="front">
-                      <h1>Front</h1>
-                    </div>
-                    <div className="back">
-                      <h1>Back</h1>
-                    </div>
-                  </div>
-                </div>
-                <div className="service">
-                  <div className="service2">
-                    <div className="front">
-                      <h1>Front</h1>
-                    </div>
-                    <div className="back">
-                      <h1>Back</h1>
-                    </div>
-                  </div>
-                </div>
-                <div className="service">
-                  <div className="service2">
-                    <div className="front">
-                      <h1>Front</h1>
-                    </div>
-                    <div className="back">
-                      <h1>Back</h1>
-                    </div>
-                  </div>
-                </div>
-                <div className="service">
-                  <div className="service2">
-                    <div className="front">
-                      <h1>Front</h1>
-                    </div>
-                    <div className="back">
-                      <h1>Back</h1>
-                    </div>
-                  </div>
-                </div>
+        <div className="serviceflex">
+          <div className="service">
+            <div className="service2">
+              <div className="front">
+                <h1>
+                  <i class="fa-solid fa-truck-fast"></i>
+                </h1>
+              </div>
+              <div className="back">
+                <p className="title">Fast Delivery</p>
+                <p className="body">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Vitae, tenetur. Esse corrupti saepe atque vel amet suscipit.
+                </p>
+              </div>
             </div>
+          </div>
+          <div className="service">
+            <div className="service2">
+              <div className="front">
+                <h1>
+                  <i class="fa-solid fa-money-bill-wave"></i>
+                </h1>
+              </div>
+              <div className="back">
+                <p className="title">Affordable goods</p>
+                <p className="body">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Vitae, tenetur. Esse corrupti saepe atque vel amet suscipit.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="service">
+            <div className="service2">
+              <div className="front">
+                <h1>
+                  <i class="fa-solid fa-handshake"></i>
+                </h1>
+              </div>
+              <div className="back">
+                <p className="title">Quality quaranteed</p>
+                <p className="body">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Vitae, tenetur. Esse corrupti saepe atque vel amet suscipit.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="service">
+            <div className="service2">
+              <div className="front">
+                <h1>
+                  <i class="fa-solid fa-globe"></i>
+                </h1>
+              </div>
+              <div className="back">
+                <p className="title">Online purchase</p>
+                <p className="body">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Vitae, tenetur. Esse corrupti saepe atque vel amet suscipit.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
